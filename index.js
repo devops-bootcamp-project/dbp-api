18df5f2711e38da55c550248669dfba969027c01
Ilyass Aktaou <ilyass.aktaou@addside.tv>
2024-08-14 21:13:27 +0100

    Enabled CORS

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 
 const app = express();
+// enable cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({ message: 'Hello World!' });
 });
 
 app.listen(port, () => {
